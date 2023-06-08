b62c33cc60be41fbf7d6d23b05040eca900ed771
Alex Vl <aleksei.s.vlasov@gmail.com>
2023-06-08 22:57:07 +0300

    v1.0.3

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL:
-    // process.env.REACT_APP_API_URL?process.env.REACT_APP_API_URL:
+    process.env.REACT_APP_API_URL?process.env.REACT_APP_API_URL:
     'http://localhost:4444',
 });
 
